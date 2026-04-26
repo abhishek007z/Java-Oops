f2ccc06246d20af8981698370517c1d5bb303e2f
abhishek007z <pandeyyabhi001@gmail.com>
2026-04-26 22:37:16 +0530

    Add notes on Open/Closed Principle with examples illustrating correct and incorrect implementations

--- INTERVIEW QUESTIONS/SOLIDNotes.java
+++ INTERVIEW QUESTIONS/SOLIDNotes.java
@@ -33,3 +33,46 @@
 //     void printReport() { }
 // }
 
+
+// // ==========================================
+// // 🧠 O — OPEN/CLOSED PRINCIPLE
+// // ==========================================
+
+// // 👉 Open for extension, closed for modification
+
+// // ❌ WRONG
+
+// class Discount {
+
+//     void getDiscount(String type) {
+
+//         if (type.equals("Diwali")) {
+//             System.out.println("10%");
+//         } else if (type.equals("NewYear")) {
+//             System.out.println("20%");
+//         }
+//     }
+// }
+
+// // 👉 New type → code modify karna padega ❌
+
+
+// // ✅ CORRECT
+
+// interface Discount {
+//     void getDiscount();
+// }
+
+// class DiwaliDiscount implements Discount {
+
+//     public void getDiscount() {
+//         System.out.println("10%");
+//     }
+// }
+
+// class NewYearDiscount implements Discount {
+
+//     public void getDiscount() {
+//         System.out.println("20%");
+//     }
+// }
